f656970c14de47dc53598231e17c7ad9cbee872b
BenediteCarter <benedterossignolle0@gmail.com>
2022-03-01 13:59:33 +0100

    modif

---
+++ demo.py
@@ -0,0 +1,17 @@
+# print("benedite")
+# nom1="Riemann"
+# nom2="Benedite"
+# print(range(9))
+
+# for i in nom1:
+#     if i=='a':
+#         print("lettere a existente")
+    # print(i)
+def square (nombre):
+    return nombre * nombre
+
+# main
+print(square(5))
+#  les listes
+alphabet=['a','b','c','d']
+print(alphabet[-1])
